aea25d14b39ec1daf055c1e54b8c3ce5dd83b8f0
shujian dong <welwin.dong@yahoo.com.cn>
2012-12-27 22:58:41 +0800

    :update delete & batch UI

---
+++ src/cn/net/yto/ui/SignDeleteActivity.java
@@ -0,0 +1,14 @@
+package cn.net.yto.ui;
+
+import cn.net.yto.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignDeleteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_delete);
+    }
+}
