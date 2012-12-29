15ed3291cc2b6f69685b8fdbc7b6e644807dcdcf
timlian <timlian@yeah.net>
2012-12-29 19:01:37 +0800

    Add Delivery Scan VO classes.

---
+++ src/cn/net/yto/vo/BaseRequestMsgVO.java
@@ -0,0 +1,11 @@
+package cn.net.yto.vo;
+
+import java.io.Serializable;
+
+public class BaseRequestMsgVO implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
+
---
+++ src/cn/net/yto/vo/BaseResponseMsgVO.java
@@ -0,0 +1,11 @@
+package cn.net.yto.vo;
+
+import java.io.Serializable;
+
+public class BaseResponseMsgVO implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
+
