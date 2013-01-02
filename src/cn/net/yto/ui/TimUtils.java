e3a5b68b868304408f67cbb56e855199a5da17ec
timlian <timlian@yeah.net>
2013-01-02 11:09:46 +0800

    Add temp files.

---
+++ src/cn/net/yto/ui/TimUtils.java
@@ -0,0 +1,31 @@
+package cn.net.yto.ui;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import android.os.Environment;
+
+public class TimUtils {
+	/**
+	* Dump the database file to external storage
+	*/
+	    static void dumpDatabase(String packageName, String fileName) throws IOException {
+	        File dbFile = new File("/data/data/" + packageName + "/databases/" + fileName);
+	        if (dbFile.exists()) {
+	            FileInputStream fis = new FileInputStream(dbFile);
+	            String outFileName = Environment.getExternalStorageDirectory() + "/" + fileName;
+	            OutputStream output = new FileOutputStream(outFileName);
+	            byte[] buffer = new byte[1024];
+	            int length;
+	            while ((length = fis.read(buffer)) > 0) {
+	                output.write(buffer, 0, length);
+	            }
+	            output.flush();
+	            output.close();
+	            fis.close();
+	        }
+	    }
+}
