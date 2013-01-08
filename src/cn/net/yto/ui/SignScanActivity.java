e3a70fbdd54cdba8b98208498185f520ada45e89
timlian <timlian@yeah.net>
2013-01-08 23:01:37 +0800

    Fix bug

--- src/cn/net/yto/ui/SignScanActivity.java
+++ src/cn/net/yto/ui/SignScanActivity.java
@@ -36,6 +36,7 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import cn.net.yto.R;
+import cn.net.yto.biz.BarcodeManager;
 import cn.net.yto.biz.SignedLogManager;
 import cn.net.yto.ui.menu.SignListBasicAdapter;
 import cn.net.yto.ui.menu.SignListItem;
@@ -367,8 +368,13 @@ public class SignScanActivity extends Activity {
         }
 
         private boolean checkInputVaules() {
-            if (TextUtils.isEmpty(mWaybillNo.getText().toString())) {
+        	final String wayBillNo = mWaybillNo.getText().toString();
+            if (TextUtils.isEmpty(wayBillNo)) {
                 ToastUtils.showToast(R.string.toast_waybillno_notify);
+                return false;
+            }
+            if(!BarcodeManager.getInstance().isWayBillNoValid(wayBillNo)) {
+            	ToastUtils.showToast(R.string.toast_invalid_waybillno);
                 return false;
             }
             if (mReceipientCheck.isChecked() && TextUtils.isEmpty(mReceipient.getText().toString())) {
